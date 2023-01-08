5f6af1a1539cd005380ed8a3db031b59bf2cee7d
Sebastian Moser <sewi.moser@gmail.com>
2023-01-08 04:15:47 +0100

    so funktioniaschts bei mia ez amoi ... des mitn npm run build

---
+++ render/game/src/Render.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useItem } from "./index";
+
+const Render = () =>{
+	const [item, setItem] = useItem();
+	pr(item)
+	function update_cards(){
+		setItem((prev) => ({...prev, cards_of_player: prev.cards_of_player + ["green_5"]}))
+	}
+   return (
+		<>
+   	<h1>
+         Welcome to the Game Render!!
+      </h1>
+		</>
+    )
+}
+
+export default Render
--- render/game/src/index.js
+++ render/game/src/index.js
@@ -1,15 +1,66 @@
 import React from "react";
-import reactDom from "react-dom";
-import App from "./App";
+import { useState } from "react";
+//import reactDom from "react-dom";
+import { createRoot } from 'react-dom/client';
+import Render from "./Render";
+//import { createContainer } from 'react-tracked';
+
+let useItem = {}
+let useThisItem = {}
+
+const hi = "hi"
+const hello = "hello from index.js"
 
 class Test extends HTMLElement {
-	  connectedCallback() {
-		      const mountPoint = document.createElement('span');
-		      this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
+	connectedCallback() {
 
-		      //const root = ReactDOM.createRoot(mountPoint);
-		      //root.render(<App />);
-		  		reactDom.render(<App />, mountPoint);
-		    }
+		const mountPoint = document.createElement('span');
+		this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
+
+		//state with react-tracked ... doesn't work
+		//const itemState = () => useState({});
+		//const { ItemProvider: SharedStateProvider, useTracked: useSharedState } = createContainer(itemState);
+		//const container = createContainer(itemState);
+		//const ItemProvider = container.Provider
+		//const useShared = container.useTracked
+
+		this.items = {}
+		this.id = mize.id_to_render
+
+		//the hook for the item, we are rendering
+		useItem = () => {
+			if (arguments.length == 0){
+				const [state, setState] = useState({_id: "hi"})
+				this.items[this.id] = {item: state, setItem: setState}
+				return [state, setState]
+
+			} else {
+				//get another item
+				//TODO
+			}
+		}
+
+		//rendering to the webcomponent
+		const root = createRoot(mountPoint); // createRoot(container!) if you use TypeScript
+
+		root.render(
+			//<ItemProvider>
+				<Render />
+			//</ItemProvider>
+		);
+	}
+
+	getItemCallback(item_raw){
+		this.item_raw = item_raw
+		//this.items[item_raw.id].setItem((prev) => ({...prev, ...item_raw.get_parsed(), hi: "hello hi"}))
+		this.items[item_raw.id].setItem((prev) => ({ ...prev, ...item_raw.get_parsed()}));
+	}
+
+	updateCallback(update){
+		this.item_raw = update.now
+		this.items[update.now.id].setItem((prev) => ({ ...prev, ...update.now.get_parsed()}));
+	}
 }
+
+export {useItem, hello}
 mize.defineRender(Test)
