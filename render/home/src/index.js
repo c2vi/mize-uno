534734e08dc19933bb2cf2d678752e55f13163f0
Sebastian Moser <sewi.moser@gmail.com>
2023-01-16 08:53:54 +0100

    copied over the new index.js, that should work with the new client

--- render/home/src/index.js
+++ render/home/src/index.js
@@ -1,103 +1,119 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 //import reactDom from "react-dom";
-import { createRoot } from 'react-dom/client'
-import Render from './Render'
+import { createRoot } from 'react-dom/client';
+import Render from "./Render";
 //import { createContainer } from 'react-tracked';
 
 let useItem = {}
 let useThisItem = {}
 
-const hello = 'hello from index.js'
+const hello = "hello from index.js"
 
 class Test extends HTMLElement {
-  connectedCallback() {
-    //state with react-tracked ... doesn't work
-    //const itemState = () => useState({});
-    //const { ItemProvider: SharedStateProvider, useTracked: useSharedState } = createContainer(itemState);
-    //const container = createContainer(itemState);
-    //const ItemProvider = container.Provider
-    //const useShared = container.useTracked
-    //the hook for the item, we are rendering
-  }
+	connectedCallback() {
 
-  getItemCallback(item_raw) {
-    const mountPoint = document.createElement('span')
-    this.attachShadow({ mode: 'open' }).appendChild(mountPoint)
+		//state with react-tracked ... doesn't work
+		//const itemState = () => useState({});
+		//const { ItemProvider: SharedStateProvider, useTracked: useSharedState } = createContainer(itemState);
+		//const container = createContainer(itemState);
+		//const ItemProvider = container.Provider
+		//const useShared = container.useTracked
 
-    this.items = {}
-    this.item_raw = item_raw
-    this.id = mize.id_to_render
 
-    useItem = (ob) => {
-      if (!ob) {
-        const [state, setState] = useState(item_raw.get_parsed())
+		//the hook for the item, we are rendering
+	}
 
-        //don't do updates the first time
-        if (!this.items[this.id]) {
-          this.items[this.id] = {
-            item: state,
-            setItem: setState,
-            do_updates: true,
-            raw: this.item_raw,
-          }
-        } else {
-          //waiting for the unparse func to be finished
-          this.items[this.id].raw.update(state)
+	getItemCallback(item_raw){
+		this.item = item_raw
 
-          this.items[this.id].item = state
-          this.items[this.id].item = setState
-        }
+		const mountPoint = document.createElement('span');
+		this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
 
-        return [state, setState]
+		this.items = {}
+		this.setItem_from_update_msg = {}
+		this.id = mize.id_to_render
 
-        //if useItem has a param
-      } else {
-        const [state, setState] = useState({})
+		useItem = (ob) => {
+			pr("====== UPDATE =====")
+			//the item rendered by this render
+			if (!ob){
+				const [state, setState] = useState(item_raw.get_parsed())
 
-        //don't do updates the first time
-        if (!this.items[ob.id]) {
-          this.items[ob.id] = {
-            item: state,
-            setItem: setState,
-            do_updates: true,
-          }
+				//don't do updates the first time
+				if (!this.items[this.id]){
+					this.items[this.id] = {item: state, setItem: setState, do_updates: true, raw: this.item}
+					this.setItem_from_update_msg[this.id] = false
 
-          //call get_item only the first time
-          mize.get_item(ob.id, (new_item) => {
-            this.items[new_item.id].raw = new_item
-            this.items[new_item.id].setItem(new_item.get_parsed())
-          })
-        } else {
-          //waiting for the unparse func to be finished
-          this.items[this.id].raw.update(state)
+				} else {
+					if (!this.setItem_from_update_msg[this.id]){
+						this.items[this.id].raw.update(state)
+					} else {
+						this.setItem_from_update_msg[this.id] = false
+					}
 
-          this.items[ob.id].item = state
-          this.items[ob.id].item = setState
-        }
+					this.items[this.id].item = state
+					this.items[this.id].item = setState
+				}
 
-        return [state, setState]
-      }
-    }
+				return [state, setState]
 
-    //rendering to the webcomponent
-    const root = createRoot(mountPoint) // createRoot(container!) if you use TypeScript
+			//if useItem has a param
+			//other items, not this.item    
+			} else {
+				const [state, setState] = useState({})
 
-    root.render(
-      //<ItemProvider>
-      <Render />
-      //</ItemProvider>
-    )
-  }
+				//don't do updates the first time
+				if (!this.items[ob.id]){
 
-  updateCallback(update) {
-    this.item_raw = update.now
-    this.items[update.now.id].setItem((prev) => ({
-      ...prev,
-      ...update.now.get_parsed(),
-    }))
-  }
+					this.items[ob.id] = {item: state, setItem: setState, do_updates: true}
+					this.setItem_from_update_msg[ob.id] = false
+					let set_item = this.items[ob.id].setItem
+
+					//call get_item only the first time
+					mize.get_item(ob.id, (new_item) => {
+						this.items[new_item.id].raw = new_item
+						this.items[new_item.id].setItem(new_item.get_parsed())
+					})
+
+					if (!mize.update_callbacks[ob.id]) {mize.update_callbacks[ob.id] = []}
+					mize.update_callbacks[ob.id].push((update) => {
+						this.setItem_from_update_msg = true
+						set_item(update.now.get_parsed())
+					})
+
+				} else {
+					if (!this.setItem_from_update_msg[ob.id]){
+						this.items[this.id].raw.update(state)
+					}
+					
+					this.items[ob.id].item = state
+					this.items[ob.id].item = setState
+				}
+
+				return [state, setState]
+				}
+		}
+
+		//rendering to the webcomponent
+		const root = createRoot(mountPoint); // createRoot(container!) if you use TypeScript
+
+		root.render(
+			//<ItemProvider>
+				<Render />
+			//</ItemProvider>
+		);
+
+	}
+
+	updateCallback(update){
+		pr("updateCallback")
+		this.item_raw = update.now
+		this.items[update.now.id].raw = update.now
+		this.setItem_from_update_msg[this.id] = true
+		this.items[update.now.id].setItem((prev) => ({ ...prev, ...update.now.get_parsed()}));
+	}
 }
 
-export { useItem, hello }
+export {useItem, hello}
 mize.defineRender(Test)
--- render/player/src/index.js
+++ render/player/src/index.js
@@ -25,56 +25,73 @@ class Test extends HTMLElement {
 	}
 
 	getItemCallback(item_raw){
+		this.item = item_raw
 
 		const mountPoint = document.createElement('span');
 		this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
 
 		this.items = {}
-		this.item_raw = item_raw
+		this.setItem_from_update_msg = {}
 		this.id = mize.id_to_render
 
 		useItem = (ob) => {
+			pr("====== UPDATE =====")
+			//the item rendered by this render
 			if (!ob){
 				const [state, setState] = useState(item_raw.get_parsed())
 
 				//don't do updates the first time
 				if (!this.items[this.id]){
-					this.items[this.id] = {item: state, setItem: setState, do_updates: true, raw: this.item_raw}
+					this.items[this.id] = {item: state, setItem: setState, do_updates: true, raw: this.item}
+					this.setItem_from_update_msg[this.id] = false
 
 				} else {
-					//waiting for the unparse func to be finished
-					this.items[this.id].raw.update(state)
-					
+					if (!this.setItem_from_update_msg[this.id]){
+						this.items[this.id].raw.update(state)
+					} else {
+						this.setItem_from_update_msg[this.id] = false
+					}
+
 					this.items[this.id].item = state
 					this.items[this.id].item = setState
 				}
 
 				return [state, setState]
 
-				//if useItem has a param
+			//if useItem has a param
+			//other items, not this.item    
+			} else {
+				const [state, setState] = useState({})
+
+				//don't do updates the first time
+				if (!this.items[ob.id]){
+
+					this.items[ob.id] = {item: state, setItem: setState, do_updates: true}
+					this.setItem_from_update_msg[ob.id] = false
+					let set_item = this.items[ob.id].setItem
+
+					//call get_item only the first time
+					mize.get_item(ob.id, (new_item) => {
+						this.items[new_item.id].raw = new_item
+						this.items[new_item.id].setItem(new_item.get_parsed())
+					})
+
+					if (!mize.update_callbacks[ob.id]) {mize.update_callbacks[ob.id] = []}
+					mize.update_callbacks[ob.id].push((update) => {
+						this.setItem_from_update_msg = true
+						set_item(update.now.get_parsed())
+					})
+
 				} else {
-					const [state, setState] = useState({})
-
-					//don't do updates the first time
-					if (!this.items[ob.id]){
-
-						this.items[ob.id] = {item: state, setItem: setState, do_updates: true}
-
-						//call get_item only the first time
-						mize.get_item(ob.id, (new_item) => {
-							this.items[new_item.id].raw = new_item
-							this.items[new_item.id].setItem(new_item.get_parsed())
-						})
-
-					} else {
-						//waiting for the unparse func to be finished
+					if (!this.setItem_from_update_msg[ob.id]){
 						this.items[this.id].raw.update(state)
-						
-						this.items[ob.id].item = state
-						this.items[ob.id].item = setState
 					}
+					
+					this.items[ob.id].item = state
+					this.items[ob.id].item = setState
+				}
 
-					return [state, setState]
+				return [state, setState]
 				}
 		}
 
@@ -90,7 +107,10 @@ class Test extends HTMLElement {
 	}
 
 	updateCallback(update){
+		pr("updateCallback")
 		this.item_raw = update.now
+		this.items[update.now.id].raw = update.now
+		this.setItem_from_update_msg[this.id] = true
 		this.items[update.now.id].setItem((prev) => ({ ...prev, ...update.now.get_parsed()}));
 	}
 }
