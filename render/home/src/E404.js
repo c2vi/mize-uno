35ef4605cd0bd46c30ce558b99c2e95d31f4aadc
lucastusch <lucas.tusch@gmail.com>
2023-01-22 19:53:03 +0100

    i think there's some dir error but idk
    what i did:
    # put all stuff in the Render.js & added a Test.js to Test if import & export works - and also checked if the index.css is globaly because i doubted that

---
+++ render/home/src/E404.js
@@ -0,0 +1,26 @@
+//import { Link } from 'react-router-dom'
+//<Link to='/'>4+</Link>
+
+const E404 = () => {
+  return (
+    <>
+      <section>
+        <div className='pt-20 flex justify-center'>
+          <div className='text-9xl font-extrabold text-violet-400 drop-shadow-lg'>
+            404
+          </div>
+        </div>
+        <div className='flex justify-center'>
+          <span className='text-lg'>
+            Sorry, that page couldn't be found! Wanna try another:{' '}
+          </span>
+          <button type='button' className='hover:scale-125 duration-200'>
+            <span className='pl-2 font-semibold text-lg'></span>
+          </button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default E404
---
+++ render/home/src/Test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Test = () => {
+  return <div className='font-extrabold text-9xl'>Test</div>
+}
+
+export default Test
